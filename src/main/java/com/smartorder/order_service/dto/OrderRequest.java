55c5b9668e31de13383df13ca806303023c71217
drishya ani <drishyaanil@9396gmail.com>
2025-12-27 00:06:51 +0530

    Added health check endpoint

---
+++ src/main/java/com/smartorder/order_service/dto/OrderRequest.java
@@ -0,0 +1,22 @@
+package com.smartorder.order_service.dto;
+
+public class OrderRequest {
+    private String productId;
+    private int quantity;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+}
