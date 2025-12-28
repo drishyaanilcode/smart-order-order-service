62bb355576b864725218be7a1ea093a80320c025
drishya ani <drishyaanil@9396gmail.com>
2025-12-28 17:46:45 +0530

    REMOVE all JPA-related imports and annotations

--- src/main/java/com/smartorder/order_service/entity/Order.java
+++ src/main/java/com/smartorder/order_service/entity/Order.java
@@ -1,13 +1,6 @@
 package com.smartorder.order_service.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
 public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String productName;
     private int quantity;
