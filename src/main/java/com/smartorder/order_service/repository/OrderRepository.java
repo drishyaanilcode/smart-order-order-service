2b840ebf7616012e42091ddf3bb310bf6a505ac7
drishya ani <drishyaanil@9396gmail.com>
2025-12-28 18:43:58 +0530

    Add Repository annotation

--- src/main/java/com/smartorder/order_service/repository/OrderRepository.java
+++ src/main/java/com/smartorder/order_service/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package com.smartorder.order_service.repository;
 
 import com.smartorder.order_service.entity.Order;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class OrderRepository {
     private List<Order> orders = new ArrayList<>();
     private Long idCounter = 1L;
